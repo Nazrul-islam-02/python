7c654ec3fc5053edcf85a269edc57d411f2fc646
Nazrul-islam-02 <nnazrulict19@gmail.com>
2026-01-11 22:20:50 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+
+
+print("Hello")
+
+mylist = ["apple", "banana", "cherry"]
+
+# print(mylist[0])
+
+# print(mylist[0])
+# print(mylist)
+
+# print(len(mylist))
+
+# mylist.push("100")
+# print(mylist)
+
+fruits = mylist
+
+print(fruits)
