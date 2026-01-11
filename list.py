a7ec7bef418f5d68b7f3b7385412f1901e67016c
Nazrul-islam-02 <nnazrulict19@gmail.com>
2026-01-11 23:06:29 +0600

    python list

---
+++ list.py
@@ -0,0 +1,30 @@
+
+nums = [12,25,46,14]
+
+# print(nums)
+
+# # print 
+# print(nums[0])
+
+# # print Rang List 
+# print(nums[0:2])
+
+name = ['navin','Nazrul',"Zim"]
+# print(name)
+
+combine = [name,nums]
+# print(combine)
+
+nums.append(10)
+
+# print(nums)
+
+# nums.clear()
+# print(nums)
+# nums.pop()
+
+nums.pop(0)
+print(nums)
+
+
+
