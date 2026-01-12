0bf4415914e4361c066793e09e860d57dbaabc96
Nazrul-islam-02 <nnazrulict19@gmail.com>
2026-01-12 19:30:47 +0600

    list

--- pythonDataType.py
+++ pythonDataType.py
@@ -19,3 +19,43 @@ b = int(a)
 
 d = complex(b,a)
 print(d)
+
+# list 
+fruits = ["apple", "banana", "mango"]
+
+# tuple
+# অপরিবর্তনযোগ্য (immutable)
+numbers = (1, 2, 3)
+
+
+# range
+# সংখ্যার সিরিজ
+r = range(1, 6)
+
+
+
+# Set Data Types
+# ইউনিক মান রাখে (ডুপ্লিকেট নেই)।
+s = {1, 2, 3, 4}
+
+# Mapping Data Type
+# 🔹 dict (Dictionary)
+# ey : value আকারে ডাটা
+student = {
+    "name": "Rahim",
+    "age": 20
+}
+
+# print(student['name'])
+
+# NoneType
+# কোনো মান নেই
+x = None
+print(type(x))
+
+li = list(range(1,11,2))
+print((li[0]))
+
+
+print(5>9 or 2 >= 9)                    
+print(5>9 and 2 >= 9)                    
