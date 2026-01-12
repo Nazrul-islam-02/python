2d2d46480b1081d4dce83125fcdf897afd386fca
Nazrul-islam-02 <nnazrulict19@gmail.com>
2026-01-13 00:00:07 +0600

    bitwise opration(

---
+++ bitWiseOperation.py
@@ -0,0 +1,41 @@
+
+
+# x = bin(9)
+# print(x)
+
+n = 789
+# print(bin(n))
+
+# print(oct(n))
+
+# print(hex(n))
+
+# deci = int("A",16)
+# print(deci)
+
+
+a = 5
+b = 3
+
+# print(a & b)
+
+# print(~ a)
+
+
+# Left Shift (<<)
+# 👉 bit বামে সরায় (× 2ⁿ)
+
+# print(a << 4)
+# print(a)
+
+# 6️⃣ Right Shift (>>)
+# 👉 bit ডানে সরায় (÷ 2ⁿ)
+
+# print(a >> 8)
+
+
+b = 12
+print(bin(b))
+
+print(bin(~b))
+
