bf9f7a95df1d716e44fe6478aafa65a3054e497b
Nazrul-islam-02 <nnazrulict19@gmail.com>
2026-01-12 15:36:56 +0600

    data type in python

---
+++ pythonDataType.py
@@ -0,0 +1,21 @@
+
+a = 2 + 3j
+# print(type(a))
+
+# print()\
+
+r = range(1, 6)
+# print(max(r))
+
+
+a = 2.09
+# print(type(a))
+
+b = int(a)
+
+# print(type(int(a)))
+# print(a)
+# print(type(b))
+
+d = complex(b,a)
+print(d)
