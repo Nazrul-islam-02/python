3aca079712c9385f6c7d3573de4ea92b1fe1342c
Nazrul-islam-02 <nnazrulict19@gmail.com>
2026-01-12 15:01:49 +0600

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,41 @@
+data = {
+    1:'Navin',
+    2:'Nazrul',
+    3:'Romjan',
+    'xxx':'Zim'
+}
+
+# Navin = data.get(1)
+
+print(data['xxx'])
+
+# non_name = data.get(22,'Not Found')
+# print(Navin)
+
+# print(non_name)
+# print(type(data))
+
+# keys = ['Navin','Kiran','Nazrul','Noman']
+# values = ['Javascripts','Java','Python','Javascripts']
+
+# data = dict(zip(keys,values))
+# print(data)
+
+
+# print(data['Nazrul'])
+
+a = 10
+b = 10
+c = 10
+
+# a, b, c are Same Adress
+
+# print(id(a))
+# print(id(b))
+# print(id(c))
+
+# print(PI)
+
+PI = 3.13
+print(type(PI))
+
