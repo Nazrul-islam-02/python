9cb78c02f196e999e43766fd23e21eee1778983f
Nazrul-islam-02 <nnazrulict19@gmail.com>
2026-01-13 16:43:35 +0600

    looop different type of patterb( triangle, peramid etc)

---
+++ loopInPy.py
@@ -0,0 +1,77 @@
+
+# i =0
+# while i<20:
+#     print("hello", i)
+#     j = 0
+#     while j < 2:
+#         print(j)
+#         j = j+1
+#     i = i+ 1
+
+# i = 0
+
+# for i in range(9):
+
+    # print(i)
+
+
+
+
+# # odd Number print without condition
+
+# for i in range(0,100,2):
+#     print(i, end=" ")
+
+
+
+# 4 cube
+
+# for i in range(0,4):
+#     for j in range(0,4):
+#         print("#", end=" ")
+    
+#     print("")
+
+
+##### create triAngle
+
+
+
+# for i in range(0,10):
+#     for j in range(0,i):
+#         print("#", end=" ")
+#     print("")
+
+
+# output
+
+# 
+# #
+# # #
+# # # #
+# # # # #
+# # # # # #
+# # # # # # #
+# # # # # # # #
+# # # # # # # # #
+
+
+for i in range(0,10):
+    for j in range(i,10):
+        print(end=" ")
+    for j in range(i):
+        print("#",end=" ")
+    print("")
+
+
+# output
+
+         # 
+        # #
+       # # #
+      # # # #
+     # # # # #
+    # # # # # #
+   # # # # # # #
+  # # # # # # # #
+ # # # # # # # # #
