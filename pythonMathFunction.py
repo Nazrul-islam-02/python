651dc677e3d774dfcbd17fa2320eda15295c8238
Nazrul-islam-02 <nnazrulict19@gmail.com>
2026-01-13 10:15:53 +0600

    Swap value not use for temporerey variable

---
+++ pythonMathFunction.py
@@ -0,0 +1,41 @@
+import math
+
+x = math.pow(3,2)
+
+# x = math.factorial(6) # 720
+x = math.floor(2.9) # 2
+
+x = math.gcd(2,8)  #gcd 2
+
+
+x = math.ceil(2.1)  #3
+
+x = math.sin(math.radians(30))
+
+a = 30
+b = 40
+
+
+
+#  Swap a and b
+# t = a
+# a = b
+# b = t
+
+
+
+#  Swap a and b 
+# a = a + b
+# b = a - b
+# a = a - b
+
+
+# another way swap a and b ---- onlly for python
+a = a^b
+b = a^b
+a = a^b
+
+# print(a)
+# print(b)
+
+print(x)
