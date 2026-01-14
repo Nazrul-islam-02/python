0cfbad7b9fb677b2f87a2666b6e2dabd263b6f15
Nazrul-islam-02 <nnazrulict19@gmail.com>
2026-01-14 09:55:15 +0600

    Different Way of Array

--- numpy1.py
+++ numpy1.py
@@ -1,13 +1,46 @@
 import numpy as np
 # import array
-print(np.__version__)
+# print(np.__version__)
 
 a = np.array([1, 2, 3])
-b = np.array([[1, 2], [3, 4]])
+b = np.array([[1, -2], [3.9, 4]])
 
 
 
-print(a + 2)      # [3 4 5]
-print(a * 2)      # [2 4 6]
+# print(a + 2)      # [3 4 5]
+# print(a * 2)      # [2 4 6]
 
-print(b *2)
+# print(b *2)
+
+# print(b[0][0])
+
+# print(b)
+
+# arr = np.linspace(1,100,100)
+# print(arr)
+
+# arr = np.full((3,4),"#")
+
+# arr = np.full((2),9)
+# arr = np.full((2,5),9)
+
+# arr = np.ones(12)
+# arr = np.ones((12,2))
+
+# arr = np.arange(1,20,2)  ### Odd number 0 to 19
+# arr = np.arange(0,21,2) ### Even number 0 to 20
+
+
+
+arr = np.eye(5)
+
+# [[1. 0. 0. 0. 0.]
+#  [0. 1. 0. 0. 0.]
+#  [0. 0. 1. 0. 0.]
+#  [0. 0. 0. 1. 0.]
+#  [0. 0. 0. 0. 1.]]
+
+
+
+print(arr)
+
