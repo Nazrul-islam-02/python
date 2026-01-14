cc8ce127d43afbe039610c2d68cdae89fa9de672
Nazrul-islam-02 <nnazrulict19@gmail.com>
2026-01-14 06:29:57 +0600

    install numpy and some use case

---
+++ numpy1.py
@@ -0,0 +1,13 @@
+import numpy as np
+# import array
+print(np.__version__)
+
+a = np.array([1, 2, 3])
+b = np.array([[1, 2], [3, 4]])
+
+
+
+print(a + 2)      # [3 4 5]
+print(a * 2)      # [2 4 6]
+
+print(b *2)
