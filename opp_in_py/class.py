8e3d5e08904c3a0c36e3907b9113dfa44cd3c88e
Nazrul-islam-02 <nnazrulict19@gmail.com>
2026-01-16 05:38:30 +0600

    Given height and base calculate Area in triangle use Class and constructure

--- opp_in_py/class.py
+++ opp_in_py/class.py
@@ -1,51 +1,69 @@
 
+# # class Student:
+# #     roll = ""
+# #     cgpa = ""
+
+
+# # Zim = Student()
+# # Zim.roll = 101
+# # Zim.cgpa = 3.54
+
+# # Rakib = Student()
+# # Rakib.roll = 102
+# # Rakib.cgpa = 3.50
+# # Rakib.cgpa = 3.50
+
+# # print(f"Roll: {Zim.roll} and CGPA: {Zim.cgpa}")
+# # print(f"Roll: {Rakib.roll} and CGPA: {Rakib.cgpa}")
+
+
+
 # class Student:
 #     roll = ""
 #     cgpa = ""
+#     name =""
 
+#     def __init__(self, roll, cgpa,name):
+#         self.roll = roll
+#         self.cgpa = cgpa
+#         self.name = name
 
-# Zim = Student()
-# Zim.roll = 101
-# Zim.cgpa = 3.54
-
-# Rakib = Student()
-# Rakib.roll = 102
-# Rakib.cgpa = 3.50
-# Rakib.cgpa = 3.50
-
-# print(f"Roll: {Zim.roll} and CGPA: {Zim.cgpa}")
-# print(f"Roll: {Rakib.roll} and CGPA: {Rakib.cgpa}")
+#     def display(self):
+#         print(f"Name:{self.name} Roll: {self.roll} and CGPA: {self.cgpa}")
 
 
 
-class Student:
-    roll = ""
-    cgpa = ""
-    name =""
+# Zim = Student(name = 'Zim',roll = 101, cgpa = 3.56)
+# name = Zim.display()
 
-    def __init__(self, roll, cgpa,name):
-        self.roll = roll
-        self.cgpa = cgpa
-        self.name = name
+# # Kim = Student()
+# # Kim.set_value(name = "kim", roll = 102, cgpa = 3.50)
+# # Kim.display()
 
-    def display(self):
-        print(f"Name:{self.name} Roll: {self.roll} and CGPA: {self.cgpa}")
+# # Rim = Student()
+# # Rim.set_value(name = "Rim", roll = 103, cgpa = 3.90)
+# # Rim.display()
 
-
-
-Zim = Student(name = 'Zim',roll = 101, cgpa = 3.56)
-name = Zim.display()
-
-# Kim = Student()
-# Kim.set_value(name = "kim", roll = 102, cgpa = 3.50)
+# Kim = Student(name = "kim", roll = 102, cgpa = 3.50)
 # Kim.display()
 
-# Rim = Student()
-# Rim.set_value(name = "Rim", roll = 103, cgpa = 3.90)
+# Rim = Student(name = "Rim", roll = 103, cgpa = 3.90)
 # Rim.display()
 
-Kim = Student(name = "kim", roll = 102, cgpa = 3.50)
-Kim.display()
 
-Rim = Student(name = "Rim", roll = 103, cgpa = 3.90)
-Rim.display()
+
+class Triangle:
+    # base = ""
+    # height = ""
+    def __init__(self,base,height):
+        self.base = base
+        self.height = height
+
+    def calculate_area(self):
+        
+        area = self.height * self.base * 0.5
+        print(area)
+
+
+t1 = Triangle(10,20)
+t1.calculate_area()
