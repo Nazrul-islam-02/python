abd28803c92781644f20f5667a87aa834f9b27c9
Nazrul-islam-02 <nnazrulict19@gmail.com>
2026-01-15 10:54:18 +0600

    Lambda Function (Anonymous), filter, map, reduce

--- factorial.py
+++ factorial.py
@@ -11,10 +11,36 @@
 
 
 
-def fact(n):
-    if(n == 0):
-        return 1
-    return n* fact(n-1)
+# def fact(n):
+#     if(n == 0):
+#         return 1
+#     return n* fact(n-1)
 
 
-print(fact(5))
+# print(fact(5))
+
+is_even = lambda x: x % 2 == 0
+print(is_even(4))  # True
+
+nums = [1, 2, 3, 4, 5]
+
+
+# filter(function, iterable)
+
+even = list(filter(lambda x: x%2 == 0,nums))
+# print(even)
+
+
+# map(function, iterable)
+
+double = list(map(lambda x: x*2, nums))
+# print(double)
+
+# print(33*9/60)
+
+
+from functools import reduce
+
+nums = [1, 2, 3, 4]
+sum = reduce(lambda a,b: a+b, nums)
+print(sum)
