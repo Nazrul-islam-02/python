e3886520754bd4eb4fb2cd50de8f15594f4a24d9
Nazrul-islam-02 <nnazrulict19@gmail.com>
2026-01-15 20:16:17 +0600

    opp class

---
+++ opp_in_py.py/class.py
@@ -0,0 +1,17 @@
+
+class Student:
+    roll = ""
+    cgpa = ""
+
+
+Zim = Student()
+Zim.roll = 101
+Zim.cgpa = 3.54
+
+Rakib = Student()
+Rakib.roll = 102
+Rakib.cgpa = 3.50
+Rakib.cgpa = 3.50
+
+print(f"Roll: {Zim.roll} and CGPA: {Zim.cgpa}")
+print(f"Roll: {Rakib.roll} and CGPA: {Rakib.cgpa}")
